e846fb79375d587890f83d3c9359ad0ef837b316
Abbaty [Muhammad] Kori <muhammad.kori@gmail.com>
2013-09-28 21:48:22 +0100

    Initialization
    First Push

---
+++ head.php
@@ -0,0 +1,13 @@
+<head>
+	<link rel="apple-touch-icon" href="touchicon.png">
+	<link rel="icon" href="favicon.png">
+	<!--[if IE]><link rel="shortcut icon" href="favicon.ico"><![endif]-->
+	<!-- or, set /favicon.ico for IE10 win -->
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="tileicon.png">
+
+
+	<title>Transmt</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Patua+One|Roboto+Slab' rel='stylesheet' type='text/css'>
+</head>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<?php include('head.php'); ?>
+	<div class="page-wrapper">
+		<div class="coming-soon">
+			<div class="width-1of2 stock-image">
+				<img src="img/stock.png" alt="Stock Image">
+			</div>
+			<div class="width-1of2 logo">
+				<img class="" src="img/transmt-Logo-with-shadow.png" alt="transmt-official-logo">
+				<p>Coming Soon</p>
+				<p class="for-android"><span>For Android</span></p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="email-launch">
+			<form>
+				<p>Fill in your email and we'll let you know when we launch</p>
+				<input class="email-input-field" type="email">
+				<input type="submit" value="I'm Interested!">
+			</form>
+		</div>
+		<div class="about">
+			<h1>About</h1>
+			<p>
+				Welcome to what will soon be the most intuitive Bulk SMS Application available.
+			</p> 
+			<p>
+				Transmt should be avaiable soon.
+			</p>
+			<p>
+				Are you tired of <i>painfully</i> copying in your contacts one after the other?
+				Well, so are we, and that's why we are building Transmt.
+				Sending Bulk Messages just got AWESOME!
+			</p>
+		</div>
+
+		<div class="footer">
+			<h2>Contact Us</h2>
+			<p>Blog</p>
+			<p><a href="mailto:someone@example.com?Subject=Coming%20Soon" target="_top">eMail</a></p>
+			<p><a href="https://www.facebook.com/pages/Transmt/398265986965469">Facebook</a></p>
+			<p><a href="https://twitter.com/transmtapp">@TransmtApp</a></p>
+			<p>Google+</p>
+			<p>&copy; <?php echo date("Y");?> <a href="http://www.transmt.com.ng">Transmt</a></p>
+		</div>
+	</div>
+</body>
+</html>
